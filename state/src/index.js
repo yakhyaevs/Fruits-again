5aad4ffab6977cea68f2ffba4fd462b902e5895a
Samar <yakhyaevdev@gmail.com>
2022-09-20 18:37:28 +0900

    added some comments

--- state/src/index.js
+++ state/src/index.js
@@ -10,6 +10,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 2-dars yakunlandi , state va component darslari takrorlandi bugun 2022.09.20
